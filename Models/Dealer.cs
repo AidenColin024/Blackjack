561da902311ab4b875518d9802b72d0bfa44f9a7
AidenColin024 <1212500@student.roc-nijmegen.nl>
2026-03-24 12:37:42 +0100

    Dealer class gemaakt klaar voor implementatie

---
+++ Models/Dealer.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Blackjack.Models
+{
+    internal class Dealer
+    {
+        public Hand hand;
+
+        public Dealer()
+        {
+            hand = new Hand();
+        }
+
+        public void AddCard(Card card)
+        {
+            hand.AddCard(card);
+        }
+
+        public void Play(Deck deck)
+        {
+            while (hand.GetTotalValue() < 17)
+            {
+                hand.AddCard(deck.DrawCard());
+            }
+        }
+    }
+}
--- obj/Debug/net10.0-windows/Blackjack.AssemblyInfo.cs
+++ obj/Debug/net10.0-windows/Blackjack.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Blackjack")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+95215bfaae33d67d01d2394467f185b33bbb4fef")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cfef636b3a2b79b479255ec02ca938afabcc39a3")]
 [assembly: System.Reflection.AssemblyProductAttribute("Blackjack")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Blackjack")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
