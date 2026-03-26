74b6a1b50793dbb4c8beca993b9c4064ccc3f4aa
AidenColin024 <1212500@student.roc-nijmegen.nl>
2026-03-26 13:56:30 +0100

    Speler class aangemaakt klaar voor implementatie

---
+++ Models/Speler.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Media;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+
+namespace Blackjack.Models
+{
+    internal class Speler
+    {
+        public Hand hand;
+
+        public Speler()
+        {
+            Hand hand = new Hand();
+        }
+
+        public void AddCard(Card card)
+        {
+            hand.AddCard(card);
+        }
+
+        public int GetTotalValue()
+        {
+            return hand.GetTotalValue();
+        }
+    }
+}
--- obj/Debug/net10.0-windows/Blackjack.AssemblyInfo.cs
+++ obj/Debug/net10.0-windows/Blackjack.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Blackjack")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+561da902311ab4b875518d9802b72d0bfa44f9a7")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e40341e77a2d50f75e59bcb94d46d9d75f578b5c")]
 [assembly: System.Reflection.AssemblyProductAttribute("Blackjack")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Blackjack")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
