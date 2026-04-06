365790b23bd8a39ca5570008926ac9669b67bb8e
AidenColin024 <1212500@student.roc-nijmegen.nl>
2026-04-06 15:46:47 +0200

    .

--- obj/Debug/net10.0-windows/Blackjack.AssemblyInfo.cs
+++ obj/Debug/net10.0-windows/Blackjack.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Blackjack")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d89dea36719dcfa2b31dd99dcf8df6402d26af6b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e1ab69b8f84b1c7f442174900d1dc788c6da052b")]
 [assembly: System.Reflection.AssemblyProductAttribute("Blackjack")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Blackjack")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
