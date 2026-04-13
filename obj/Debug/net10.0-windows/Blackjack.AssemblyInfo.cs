94983bd5b9095aad994782008460aa0287ce0f5a
AidenColin024 <1212500@student.roc-nijmegen.nl>
2026-04-13 14:18:14 +0200

    test splitsen
    is een beetje hetzelfde als in 1 keer blackjack krijgen. het gebeurt niet vaak

--- obj/Debug/net10.0-windows/Blackjack.AssemblyInfo.cs
+++ obj/Debug/net10.0-windows/Blackjack.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Blackjack")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5ea99be2ee2ffb3a4ecadf0afec442616abd22bd")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+696ff08a4ac06a0e7b08acf9e0261762abb769dd")]
 [assembly: System.Reflection.AssemblyProductAttribute("Blackjack")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Blackjack")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
